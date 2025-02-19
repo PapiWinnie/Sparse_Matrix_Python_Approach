e58770a9906373dfade2e1a71eecf5f028fee233
Winston Nji <winston00russel@gmail.com>
2025-02-19 16:16:47 +0200

    Implementing find_txt_files to locate matrix input files

--- dsa/sparse_matrix/code/src/main.py
+++ dsa/sparse_matrix/code/src/main.py
@@ -6,3 +6,8 @@ def get_project_root():
     current_dir = os.path.dirname(os.path.abspath(__file__))
     return os.path.dirname(os.path.dirname(current_dir))
 
+def find_txt_files(folder_path):
+    txt_files = [f for f in os.listdir(folder_path) if f.endswith('.txt') and f != 'result.txt']
+    if len(txt_files) < 2:
+        raise FileNotFoundError("Error: Not enough .txt files found in sample_inputs folder.")
+    return [os.path.join(folder_path, txt_files[i]) for i in range(2)]
